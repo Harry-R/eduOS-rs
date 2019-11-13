66222e83ec9765a9415fa6f950a896b1fea0ed4e
Leonard Rapp <leonard.rapp@rwth-aachen.de>
2019-11-13 11:38:19 +0100

    aarch64: fix gic initialisation, add helper functions

--- src/arch/aarch64/irq.rs
+++ src/arch/aarch64/irq.rs
@@ -18,54 +18,54 @@ use scheduler::reschedule;
 const GICR_BASE: u64 = 0;
 
 /* GIC Distributor interface register offsets that are common to GICv3 & GICv2 */
-const GICD_BASE: i64 = 0x08000000;
-const GICC_BASE: i64 = GICD_BASE + GICD_SIZE;
-const GIC_SIZE: i64 = GICD_SIZE + GICC_SIZE;
-const GICD_SIZE: i64 = 0x010000;
-const GICC_SIZE: i64 = 0x010000;
+const GICD_BASE: u64 = 0x08000000;
+const GICC_BASE: u64 = GICD_BASE + GICD_SIZE;
+const GIC_SIZE: u64 = GICD_SIZE + GICC_SIZE;
+const GICD_SIZE: u64 = 0x010000;
+const GICC_SIZE: u64 = 0x010000;
 
-const GICD_CTLR: i32 = 0x0;
-const GICD_TYPER: i32 = 0x4;
-const GICD_IIDR: i32 = 0x8;
-const GICD_IGROUPR: i32 = 0x80;
-const GICD_ISENABLER: i32 = 0x100;
-const GICD_ICENABLER: i32 = 0x180;
-const GICD_ISPENDR: i32 = 0x200;
-const GICD_ICPENDR: i32 = 0x280;
-const GICD_ISACTIVER: i32 = 0x300;
-const GICD_ICACTIVER: i32 = 0x380;
-const GICD_IPRIORITYR: i32 = 0x400;
-const GICD_ITARGETSR: i32 = 0x800;
-const GICD_ICFGR: i32 = 0xc00;
-const GICD_NSACR: i32 = 0xe00;
-const GICD_SGIR: i32 = 0xF00;
+const GICD_CTLR: u64 = 0x0;
+const GICD_TYPER: u64 = 0x4;
+const GICD_IIDR: u64 = 0x8;
+const GICD_IGROUPR: u64 = 0x80;
+const GICD_ISENABLER: u64 = 0x100;
+const GICD_ICENABLER: u64 = 0x180;
+const GICD_ISPENDR: u64 = 0x200;
+const GICD_ICPENDR: u64 = 0x280;
+const GICD_ISACTIVER: u64 = 0x300;
+const GICD_ICACTIVER: u64 = 0x380;
+const GICD_IPRIORITYR: u64 = 0x400;
+const GICD_ITARGETSR: u64 = 0x800;
+const GICD_ICFGR: u64 = 0xc00;
+const GICD_NSACR: u64 = 0xe00;
+const GICD_SGIR: u64 = 0xF00;
 
-const GICD_CTLR_ENABLEGRP0: i32 = (1 << 0);
-const GICD_CTLR_ENABLEGRP1: i32 = (1 << 1);
+const GICD_CTLR_ENABLEGRP0: u32 = (1 << 0);
+const GICD_CTLR_ENABLEGRP1: u32 = (1 << 1);
 
 /* Physical CPU Interface registers */
-const GICC_CTLR: i32 = 0x0;
-const GICC_PMR: i32 = 0x4;
-const GICC_BPR: i32 = 0x8;
-const GICC_IAR: i32 = 0xC;
-const GICC_EOIR: i32 = 0x10;
-const GICC_RPR: i32 = 0x14;
-const GICC_HPPIR: i32 = 0x18;
-const GICC_AHPPIR: i32 = 0x28;
-const GICC_IIDR: i32 = 0xFC;
-const GICC_DIR: i32 = 0x1000;
-const GICC_PRIODROP: i32 = GICC_EOIR;
+const GICC_CTLR: u64 = 0x0;
+const GICC_PMR: u64 = 0x4;
+const GICC_BPR: u32 = 0x8;
+const GICC_IAR: u32 = 0xC;
+const GICC_EOIR: u32 = 0x10;
+const GICC_RPR: u32 = 0x14;
+const GICC_HPPIR: u32 = 0x18;
+const GICC_AHPPIR: u32 = 0x28;
+const GICC_IIDR: u32 = 0xFC;
+const GICC_DIR: u32 = 0x1000;
+const GICC_PRIODROP: u32 = GICC_EOIR;
 
-const GICC_CTLR_ENABLEGRP0: i32 = (1 << 0);
-const GICC_CTLR_ENABLEGRP1: i32 = (1 << 1);
-const GICC_CTLR_FIQEN: i32 = (1 << 3);
-const GICC_CTLR_ACKCTL: i32 = (1 << 2);
+const GICC_CTLR_ENABLEGRP0: u32 = (1 << 0);
+const GICC_CTLR_ENABLEGRP1: u32 = (1 << 1);
+const GICC_CTLR_FIQEN: u32 = (1 << 3);
+const GICC_CTLR_ACKCTL: u32 = (1 << 2);
 
-const MAX_HANDLERS: i32 = 256;
-const RESCHED_INT: i32 = 1;
+const MAX_HANDLERS: u32 = 256;
+const RESCHED_INT: u32 = 1;
 
 // TODO: find out, what "EINVAL" is
-const EINVAL: i32 = 42;
+const EINVAL: u32 = 42;
 
 // This is dummy, has to be code pointer array -> Ignore first, maybe implement later,
 // if wee need more than one handler
@@ -79,58 +79,56 @@ pub fn trigger_schedule() {
 
 fn gicd_read(off: u64) -> u32 {
 	let value;
-	unsafe { asm!("ldar w0, [x1]" : "=r"(value) : "{x1}"(GICD_BASE +off as i64) : "memory")};
+	unsafe { asm!("ldar w0, [x1]" : "=r"(value) : "{x1}"(GICD_BASE +off as u64) : "memory")};
 	return value;
 }
 
 fn gicd_write(off: u64, value: u32) -> () {
-	unsafe { asm!("str w0, [x1]" : : "rZ" (value), "{x1}" (GICD_BASE +off as i64) : "memory")};
+	unsafe { asm!("str w0, [x1]" : : "rZ" (value), "{x1}" (GICD_BASE +off as u64) : "memory")};
 }
 
 fn gicc_read(off: u64) -> u32 {
 	let value;
-	unsafe{asm!("ldar w0, [x1]" : "=r"(value) : "{x1}"(GICC_BASE +off as i64) : "memory")};
+	unsafe{asm!("ldar w0, [x1]" : "=r"(value) : "{x1}"(GICC_BASE +off as u64) : "memory")};
 	return value;
 }
 
 fn gicc_write(off: u64, value: u32) {
-	unsafe{asm!("str w0, [x1]" : : "rZ" (value), "{x1}" (GICC_BASE +off as i64) : "memory")};
+	unsafe{asm!("str w0, [x1]" : : "rZ" (value), "{x1}" (GICC_BASE +off as u64) : "memory")};
 }
 
 
-fn unmask_interrupt(vector: u32) -> i32{
+fn unmask_interrupt(vector: u32) -> Result<(),()>{
     if vector >= (((gicd_read(GICD_TYPER as u64) & 0x1f) + 1) * 32) {
-		return -EINVAL;
+		return Err(());
 	}
 	// TODO: Implement with spin crate
     // spinlock_irqsave_lock(&mask_lock);
-    gic_set_enable(vector, true);
+    gic_set_enable(vector as u64, true);
     // spinlock_irqsave_unlock(&mask_lock);
-
-    return 0;
+   Ok(())
 }
 
-fn mask_interrupt(vector: u32) -> i32 {
+fn mask_interrupt(vector: u32) -> Result<(),()> {
     if vector >= (((gicd_read(GICD_TYPER as u64) & 0x1f) + 1) * 32) {
-		return -EINVAL;
+		return Err(());
 	}
 	// TODO: Implement with spin crate
     // spinlock_irqsave_lock(&mask_lock);
-    gic_set_enable(vector, false);
+    gic_set_enable(vector as u64, false);
     // spinlock_irqsave_unlock(&mask_lock);
-
-    return 0;
+    Ok(())
 }
 
 
-fn gic_set_enable(vector: u32, enable: bool) {
-    if enable {
-        let regoff: u64 = (GICD_ISENABLER + (4 * (vector / 32) as i32)) as u64;
-        gicd_write(regoff, (gicd_read(regoff) | (1 << (vector % 32))) as u32);
-    } else {
-        let regoff :u64 = (GICD_ICENABLER + (4 * (vector / 32) as i32)) as u64;;
-        gicd_write(regoff, (gicd_read(regoff) | (1 << (vector % 32))) as u32);
-    }
+fn gic_set_enable(vector: u64, enable: bool) {
+	if enable == true {
+		let regoff = GICD_ISENABLER + 4 * (vector / 32);
+		gicd_write(regoff, gicd_read(regoff) | 1 << (vector % 32));
+	} else {
+		let regoff = GICD_ICENABLER + 4 * (vector / 32);
+		gicd_write(regoff, gicd_read(regoff) | 1 << (vector % 32));
+	}
 }
 
 /// Enable Interrupts
@@ -138,19 +136,51 @@ fn gic_set_enable(vector: u32, enable: bool) {
 pub fn irq_enable() {
     // Global enable signalling of interrupt from the cpu interface
 	gicc_write(GICC_CTLR as u64, (GICC_CTLR_ENABLEGRP0 | GICC_CTLR_ENABLEGRP1 | GICC_CTLR_FIQEN | GICC_CTLR_ACKCTL) as u32);
-	gicd_enable()
 }
 
 fn gicd_enable() {
 	println!("global enable forwarding from gicd");
 	// Global enable forwarding interrupts from distributor to cpu interface
-	gicd_write(GICD_CTLR as u64, (GICD_CTLR_ENABLEGRP0 | GICD_CTLR_ENABLEGRP1) as u32);
+	gicd_write(GICD_CTLR, (GICD_CTLR_ENABLEGRP0 | GICD_CTLR_ENABLEGRP1) as u32);
+}
+
+fn gicd_disable() {
+	// Global disable forwarding interrupts from distributor to cpu interface
+	gicd_write(GICD_CTLR, 0);
+}
+
+fn gicc_enable() {
+	println!("global enable forwarding from gicc");
+	// Global enable signalling of interrupt from the cpu interface
+    gicc_write(GICC_CTLR, GICC_CTLR_ENABLEGRP0 | GICC_CTLR_ENABLEGRP1 | GICC_CTLR_FIQEN | GICC_CTLR_ACKCTL);
+}
+
+fn gicc_disable() {
+	// Global disable signalling of interrupt from the cpu interface
+	gicc_write(GICC_CTLR, 0);
 }
 
 /// Disable Interrupts
 pub fn irq_disable() {
 	// Global disable signalling of interrupt from the cpu interface
 	gicc_write(GICC_CTLR as u64, 0);
+}
+
+fn gicc_set_priority(priority: u32) {
+	gicc_write(GICC_PMR, priority & 0xFF);
+}
+
+pub fn init() {
+
+	println!("initialize interrupt controller");
+	gicd_enable();
+	gicc_set_priority(0xF0);
+	gicc_enable();
+
+	let _ = unmask_interrupt(RESCHED_INT);
+
+    // enable interrupts, clear A, I, F flags
+    unsafe { asm!("msr daifclr, 0b111" ::: "memory") };
 }
 
 #[no_mangle]
